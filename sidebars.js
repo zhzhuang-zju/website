80ad2e905557c7c9e76d30572337e54c9850c753
yinfengyuan <sipeng@whu.edu.cn>
2026-09-02 11:29:23 +0800

    docs: add v1.18 to v1.19 upgrading guide
    Signed-off-by: yinfengyuan <sipeng@whu.edu.cn>

--- sidebars.js
+++ sidebars.js
@@ -254,7 +254,8 @@ module.exports = {
                         "administrator/upgrading/v1.14-v1.15",
                         "administrator/upgrading/v1.15-v1.16",
                         "administrator/upgrading/v1.16-v1.17",
-                        "administrator/upgrading/v1.17-v1.18"
+                        "administrator/upgrading/v1.17-v1.18",
+                        "administrator/upgrading/v1.18-v1.19"
                     ],
                 },
                 {
